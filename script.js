5e1542da1e0edc7cf8de6e01792759684c4e0ed3
je33ica <je331687@gmail.com>
2020-11-27 15:31:08 +0000

    checking correct route for Temp, logging to console, needs converting from Kelvin to Celsius

--- script.js
+++ script.js
@@ -26,12 +26,9 @@
  var UVindex
  var city = "london"  //"" hardcoding for now to continue to check
  var APIKey = "8bf86a426ab2a44eddf367d412a04ad4";
- //use this one for the weather forcast 
+ 
 
- var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`
- console.log(queryURL);
-
-
+ //<-------------all of this for th UV-------------->
 //   $("#form").on('submit', function (e) {
 //     e.preventDefault();
 //     var city = "london"
@@ -41,17 +38,51 @@
       url: queryURL,
       method: "GET"
     }).then(function (response) {
-  
+  // need long and lat to make UV api call
       var longitude = response.coord.lon
       var latitude = response.coord.lat
   console.log(latitude, longitude)
 
-
 var UVqueryURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${latitude}&lon=${longitude}&appid=${APIKey}`
 $.ajax({
     url: UVqueryURL,
-    method: "GET"
+    method: "GET",
+    dataType: "json",
   }).then(function (response) {
      // response from UV api call but how do i make that a UV reading ?
-    console.log(response)})
-})
+    console.log(response.value)})
+// appears in console but not on page( well undefined)because it's a lil bitch!
+    $("#UV-index").text("UV Index: " + response.value)
+   //badges for UV ratings- low,warning,high
+    if(response.value < 3){
+    $("#UV-index").removeClass()
+    $("#UV-index").addClass("badge badge-success")
+    }
+    if(response.value > 3 && response.value < 7){
+    $("#UV-index").removeClass()
+    $("#UV-index").addClass("badge badge-warning")
+    }
+    if(response.value > 7){
+      $("#UV-index").removeClass()
+      $("#UV-index").addClass("badge badge-danger")
+      }
+  });
+//<-----------------UV-------------->   
+
+
+
+
+//use this one for the weather forcast 
+ var forcastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`
+ $.ajax({
+  url: forcastURL,
+  method: "GET"
+}).then(function (response) {
+// need long and lat to make UV api call
+  var temp = response.list[0].main.temp
+  //var icon = response
+console.log( temp) } )
+
+// list.dt.weather.id.icon
+// list .dt .main.temp
+// list.dt.wind. speed 
